2f2635ee71101c932e842ea99e736bfdf17f173a
Prem-599 <prematkapurwar599@gmail.com>
2023-08-22 14:54:14 +0530

    java program of harmonic series.

--- harmonicSeries.java
+++ harmonicSeries.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class harmonicSeries {
     public static void main(String[] args) {
 
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter any number");
+            double n = scanner.nextDouble();
+            int num = 5;
+            double result = 0.0;
+            System.out.println("The harmonic series is: ");
+            while (num > 0) {
+                result = result + (double) 1 / num;
+                num--;
+                System.out.print(result + ", ");
+
+
+            }
+        }
     }
-}
+
