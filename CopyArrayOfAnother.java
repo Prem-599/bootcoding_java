332d0eed38715cc8686f222e995cdb7c08fa006a
Prem-599 <prematkapurwar599@gmail.com>
2023-08-22 14:51:12 +0530

    java program of array o copy of another Array.

---
+++ CopyArrayOfAnother.java
@@ -0,0 +1,12 @@
+public class CopyArrayOfAnother {
+    public static void main(String[] args) {
+        int a[] = {1, 2, 3, 4, 5};
+        int n=a.length;
+        int b=0;
+            b=n;
+        System.out.println("content of first array "+ a.length);
+        for (int i=1;i<=b;i++){
+            System.out.println(i);
+        }
+    }
+}
